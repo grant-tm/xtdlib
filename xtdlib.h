9169a87b21c0e66b834b1e162e1da838e9686579
grant-tm <grant.t.morgan@gmail.com>
2025-08-06 11:22:45 -0500

    add top level header

---
+++ xtdlib.h
@@ -0,0 +1,12 @@
+#ifndef XTDLIB_H
+#define XTDLIB_H
+
+#include "xbinary.h"
+#include "xmath.h"
+#include "xmemory.h"
+#include "xstorage.h"
+#include "xstring.h"
+#include "xstyle.h"
+#include "xsystem.h"
+
+#endif // XTDLIB_H
